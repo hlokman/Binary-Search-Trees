29570d8c7f444676be62d1dde5443df3ce74db6e
hlokman <abdallahlokmanhenni@gmail.com>
2023-11-05 05:32:55 +0100

    Switch to class version to fix the issues

--- binary-tree.js
+++ binary-tree.js
@@ -1,62 +1,52 @@
-const Node = (value, leftChild = null, rightChild = null) => {
-  return { value, leftChild, rightChild };
-};
+class Node {
+  constructor(value, leftChild = null, rightChild = null) {
+    this.value = value;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+  }
+}
 
-const Tree = (array = []) => {
-  //----------BUILD TREE
-  function buildTree(array) {
-    if (array.length === 0) {
-      return null;
-    }
-    //sort the array
-    array.sort((a, b) => a - b);
-    //remove the replicate (function)
-    const removeDuplicates = (array) => {
-      let unique = [];
-      array.forEach((item) => {
-        if (!unique.includes(item)) {
-          unique.push(item);
-        }
-      });
-      return unique;
-    };
-    const sortedArray = removeDuplicates(array);
+class Tree {
+  constructor(array) {
+    this.sortedArray = [...new Set(array)].sort((a, b) => a - b);
+    this.root = this.buildTree(this.sortedArray); //that's why you don't write this.sortedArray *
+  }
 
-    const middle = Math.floor((sortedArray.length - 1) / 2);
-    let root = Node(
+  buildTree(sortedArray) {
+    //* HERE
+    if (sortedArray.length === 0) return null;
+    let middle = Math.floor((sortedArray.length - 1) / 2);
+    const newNode = new Node(
       sortedArray[middle],
-      buildTree(sortedArray.slice(0, middle)),
-      buildTree(sortedArray.slice(middle + 1))
+      this.buildTree(sortedArray.slice(0, middle)),
+      this.buildTree(sortedArray.slice(middle + 1))
     );
 
-    return root;
+    return newNode;
   }
-  let root = buildTree(array); //! to put root in the return statement and make the (arranged) tree available when Tree(array) is called (with an array within)
 
-  //----------INSERT
-  function insertNode(value, node = root) {
+  insertNode(value, node = this.root) {
     if (node.value === value) {
       return;
     } //if value < node.value
     else if (node.value > value) {
       if (node.leftChild) {
         //call the function recursively until the node is "alone": if you add a Node, the node supposed to be above is necessarily "alone" so doesn't have a left or right child yet
-        insertNode(value, node.leftChild);
+        this.insertNode(value, node.leftChild);
       } else {
-        node.leftChild = Node(value);
+        node.leftChild = new Node(value);
       }
     } //if value > node.value
     else {
       if (node.rightChild) {
-        insertNode(value, node.rightChild);
+        this.insertNode(value, node.rightChild);
       } else {
-        node.rightChild = Node(value);
+        node.rightChild = new Node(value);
       }
     }
   }
 
-  //----------FIND
-  function find(value, node = root) {
+  find(value, node = this.root) {
     //base case: if the value is find (node.value === value) return node (which shows value, left and right child)
     // or if the value doesn't exist return node (since in that case node === null)
     if (node === null || node.value === value) {
@@ -65,13 +55,13 @@ const Tree = (array = []) => {
 
     //call the function recursively
     if (value > node.value) {
-      return find(value, node.rightChild);
+      return this.find(value, node.rightChild);
     } else if (value < node.value) {
-      return find(value, node.leftChild);
+      return this.find(value, node.leftChild);
     }
   }
 
-  function findMinValue(node) {
+  findMinValue(node = this) {
     if (node === null) {
       return null;
     }
@@ -83,17 +73,16 @@ const Tree = (array = []) => {
     return min;
   }
 
-  //----------DELETE
-  function deleteNode(value, node = root) {
-    if (node === null || find(value) === null) {
+  deleteNode(value, node = this.root) {
+    if (node === null || this.find(value) === null) {
       return null;
     }
 
     //in order to go through the nodes
     if (value > node.value) {
-      node.rightChild = deleteNode(value, node.rightChild);
+      node.rightChild = this.deleteNode(value, node.rightChild);
     } else if (value < node.value) {
-      node.leftChild = deleteNode(value, node.leftChild);
+      node.leftChild = this.deleteNode(value, node.leftChild);
     } else {
       //until value = node.value
       //Then there are 2 cases: 1st = when one or no child
@@ -103,9 +92,9 @@ const Tree = (array = []) => {
         return node.leftChild;
       }
       //2nd = when two children
-      node.value = findMinValue(node.rightChild);
-      node.rightChild = deleteNode(
-        findMinValue(node.rightChild),
+      node.value = this.findMinValue(node.rightChild);
+      node.rightChild = this.deleteNode(
+        this.findMinValue(node.rightChild),
         node.rightChild
       );
     }
@@ -113,39 +102,35 @@ const Tree = (array = []) => {
     return node;
   }
 
-  //----------LEVEL ORDER (Breadth-first traversal)
-  //use queue logic
-  //if no function is given, return array of values
-  function levelOrder(callback) {
-    const queue = [root];
+  levelOrder(callback) {
+    const queue = [this.root];
     const result = [];
     //define default callback if no callback entered
     if (typeof callback === "undefined") {
-      callback = (node) => {
+      callback = (node = this.root) => {
         result.push(node.value);
       };
     }
 
     while (queue.length !== 0) {
       const node = queue.shift();
+      callback(node); //callback (might be default one) used
       if (node.leftChild !== null) {
         queue.push(node.leftChild);
       }
       if (node.rightChild !== null) {
         queue.push(node.rightChild);
       }
-      callback(node); //callback (might be default one) used
     }
 
     return result;
   }
 
-  //----------DEPTH-FIRST TRAVERSAL 1 = Preorder
-  function preorder(callback, node = root) {
+  preorder(callback, node = this.root) {
     const result = [];
     //define default callback if no callback entered
     if (typeof callback === "undefined") {
-      callback = (node) => {
+      callback = (node = this.root) => {
         result.push(node.value);
       };
     }
@@ -154,17 +139,16 @@ const Tree = (array = []) => {
       return result;
     }
     callback(node); //callback (might be default one) used
-    preorder(callback, node.leftChild);
-    preorder(callback, node.rightChild);
+    this.preorder(callback, node.leftChild);
+    this.preorder(callback, node.rightChild);
     return result;
   }
 
-  //----------DEPTH-FIRST TRAVERSAL 2 = Inorder
-  function inorder(callback, node = root) {
+  inorder(callback, node = this.root) {
     const result = [];
     //define default callback if no callback entered
     if (typeof callback === "undefined") {
-      callback = (node) => {
+      callback = (node = this.root) => {
         result.push(node.value);
       };
     }
@@ -172,18 +156,17 @@ const Tree = (array = []) => {
     if (node === null) {
       return result;
     }
-    inorder(callback, node.leftChild);
+    this.inorder(callback, node.leftChild);
     callback(node); //callback (might be default one) used
-    inorder(callback, node.rightChild);
+    this.inorder(callback, node.rightChild);
     return result;
   }
 
-  //----------DEPTH-FIRST TRAVERSAL 3 = Postorder
-  function postorder(callback, node = root) {
+  postorder(callback, node = this.root) {
     const result = [];
     //define default callback if no callback entered
     if (typeof callback === "undefined") {
-      callback = (node) => {
+      callback = (node = this.root) => {
         result.push(node.value);
       };
     }
@@ -191,24 +174,26 @@ const Tree = (array = []) => {
     if (node === null) {
       return result;
     }
-    postorder(callback, node.leftChild);
-    postorder(callback, node.rightChild);
+    this.postorder(callback, node.leftChild);
+
+    this.postorder(callback, node.rightChild);
     callback(node); //callback (might be default one) used
     return result;
   }
 
-  //----------HEIGHT
-  function height(node = root) {
+  height(node = this.root) {
     if (node === null) {
       return 0; //base case
     } else {
-      return Math.max(height(node.leftChild) + 1, height(node.rightChild) + 1);
+      return Math.max(
+        this.height(node.leftChild) + 1,
+        this.height(node.rightChild) + 1
+      );
     }
   }
 
-  //----------DEPTH
-  function depth(value, rootNode = root) {
-    if (find(value, rootNode) === null) {
+  depth(value, rootNode = this.root) {
+    if (this.find(value, rootNode) === null) {
       return -1;
     }
 
@@ -216,69 +201,53 @@ const Tree = (array = []) => {
       return 0; //
     }
     return Math.max(
-      depth(value, rootNode.leftChild) + 1,
-      depth(value, rootNode.rightChild) + 1
+      this.depth(value, rootNode.leftChild) + 1,
+      this.depth(value, rootNode.rightChild) + 1
     );
   }
 
-  //----------ISBALANCED
-  function isBalanced(node = root) {
+  isBalanced(node = this.root) {
     if (node === null) {
       return true;
     }
     return (
       //below: return boolean
-      Math.abs(height(node.leftChild) - height(node.rightChild)) <= 1 &&
-      isBalanced(node.leftChild) &&
-      isBalanced(node.rightChild)
+      Math.abs(this.height(node.leftChild) - this.height(node.rightChild)) <=
+        1 &&
+      this.isBalanced(node.leftChild) &&
+      this.isBalanced(node.rightChild)
     );
   }
 
-  //----------REBALANCE
-  function rebalance() {
-    this.root = buildTree(inorder()); //in order to change the tree.root when we call the tree
-    root = buildTree(inorder()); //in order to redefine the ""actual"" root value (first defined with let root = buildTree(array) above and which is used in the other functions)
+  rebalance() {
+    this.root = this.buildTree(this.inorder());
   }
 
-  return {
-    root,
-    buildTree,
-    insertNode,
-    find,
-    deleteNode,
-    levelOrder,
-    preorder,
-    inorder,
-    postorder,
-    height,
-    depth,
-    isBalanced,
-    rebalance,
-  };
-};
-
-//----------PRINT THE TREE
-//console.log the tree in a structured format
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null) {
-    return;
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.rightChild !== null) {
+      this.prettyPrint(
+        node.rightChild,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.leftChild !== null) {
+      this.prettyPrint(
+        node.leftChild,
+        `${prefix}${isLeft ? "    " : "│   "}`,
+        true
+      );
+    }
   }
-  if (node.rightChild !== null) {
-    prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-  if (node.leftChild !== null) {
-    prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  }
-};
-
-//----------SCRIPT
-function randomArray(size) {
-  return Array.from({ length: size }, () => Math.floor(Math.random() * 100)); //will call Math.floor for each element (*100 to get random numbers < 100)
 }
 
-const newTree = Tree(randomArray(18));
-prettyPrint(newTree.root);
+//----------Script
+const newTree = new Tree(randomArray(18));
+newTree.prettyPrint();
 console.log("Balanced:", newTree.isBalanced());
 console.log("Level-order traversal:", newTree.levelOrder());
 console.log("Preorder traversal:", newTree.preorder());
@@ -290,10 +259,10 @@ newTree.insertNode(364);
 newTree.insertNode(874);
 newTree.insertNode(659);
 newTree.insertNode(2111);
-prettyPrint(newTree.root);
+newTree.prettyPrint();
 console.log("Balanced:", newTree.isBalanced());
 newTree.rebalance();
-prettyPrint(newTree.root);
+newTree.prettyPrint();
 console.log("Balanced:", newTree.isBalanced());
 console.log("Level-order traversal:", newTree.levelOrder());
 console.log("Preorder traversal:", newTree.preorder());
